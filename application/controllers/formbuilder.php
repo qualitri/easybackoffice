87d2fa0b746a4ecc8da447a50aac85f7d5f631c4
carlos calderon <carlos.calderon@qualitri.com>
2013-08-12 15:49:13 -0500

    codeigniter project

---
+++ application/controllers/formbuilder.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class FormBuilder extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('addfields');
+	}
+	/*public function */
+}
---
+++ application/views/addfields - copia.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Bienvenido a Qualitri Form</title>
+</head>
+<body>
+
+<div id="container">
+	<form method="post">
+		<h1>Step 1 - list all form fields</h1>
+		<label for="txtarea">fields</label>
+		<textarea id="txtarea" name="txtarea" cols="40" rows="5" ></textarea>
+		<input type="submit" value="Submit form" />
+	</form>
+
+</div>
+
+</body>
+</html>
