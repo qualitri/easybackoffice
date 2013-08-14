e56f048080b297889af9665847b950394443fe2d
carlos calderon <carlos.calderon@qualitri.com>
2013-08-14 10:09:26 -0500

    adding views

--- application/views/export_download.php
+++ application/views/export_download.php
@@ -2,9 +2,14 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Export</title>
+	<title>Export Download</title>
 </head>
 <body>
-
+	<div id="container">
+		<form id="field-properties" method="post">
+			<h1>Download Files</h1>
+			<input type="submit" value="Submit form" />
+		</form>
+	</div>
 </body>
 </html>
